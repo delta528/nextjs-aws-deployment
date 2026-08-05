8017148b3bfde6b831434a738a97d622fbbb63e4
Dinesh <d.nabkoty@gmail.com>
2026-08-05 19:44:47 +0530

    Add Elastic Beanstalk configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
-import Link from "next/link";
+export const dynamic = "force-dynamic";
 
-export default function Home() {
+import Link from "next/link";
+import flagsmith from "@/utils/flagsmith";
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const flags = await flagsmith.getEnvironmentFlags();
+
+  console.log("Search enabled:", flags.isFeatureEnabled("search"));
+  console.log("All flags:", flags);
+
   return (
     <main className="main">
       <div>
         <h2>Hello World</h2>
       </div>
+
       <Link href="/about">About</Link>
+
+      <br />
+
+      {flags.isFeatureEnabled("search") && (
+        <input placeholder="Search" />
+      )}
     </main>
   );
 }
+
